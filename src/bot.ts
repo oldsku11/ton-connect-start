2449453a9a9b4c7bf3363e1fc27fc7d067d00765
oldsku11 <muradov86@gmail.com>
2023-09-16 20:35:43 +0200

    removed unused vars

--- src/bot.ts
+++ src/bot.ts
@@ -2,7 +2,5 @@ import TelegramBot from 'node-telegram-bot-api'
 import * as process from 'process'
 
 const token = process.env.TELEGRAM_BOT_TOKEN!
-const message = process.env.REPLY_MESSAGE!
 
 export const bot = new TelegramBot(token, { polling: true })
-export { message }
