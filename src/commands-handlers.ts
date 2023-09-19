ddc2b69f40cd09e9428eb897d52ec57b4d26252c
oldsku11 <muradov86@gmail.com>
2023-09-19 20:31:59 +0200

    implemented transaction sending

--- src/commands-handlers.ts
+++ src/commands-handlers.ts
@@ -3,6 +3,7 @@ import { getWallets } from './ton-connect/wallets'
 import QRCode from 'qrcode'
 import TelegramBot from 'node-telegram-bot-api'
 import { getConnector } from './ton-connect/connector'
+import { UserRejectsError, isWalletInfoRemote } from '@tonconnect/sdk'
 
 export async function handleConnectCommand(
   msg: TelegramBot.Message
@@ -39,4 +40,69 @@ export async function handleConnectCommand(
       ],
     },
   })
+}
+
+export async function handleSendTXCommand(
+  msg: TelegramBot.Message
+): Promise<void> {
+  const chatId = msg.chat.id
+
+  const connector = getConnector(chatId)
+
+  await connector.restoreConnection()
+  if (!connector.connected) {
+    await bot.sendMessage(chatId, 'Connect wallet to send transaction')
+    return
+  }
+
+  const wallets = await connector.getWallets()
+
+  connector
+    .sendTransaction({
+      validUntil: Math.round(Date.now() / 1000) + 600, // timeout is SECONDS
+      messages: [
+        {
+          amount: '1000000',
+          address:
+            '0:0000000000000000000000000000000000000000000000000000000000000000',
+        },
+      ],
+    })
+    .then(() => {
+      bot.sendMessage(chatId, `Transaction sent successfully`)
+    })
+    .catch((e) => {
+      if (e instanceof UserRejectsError) {
+        bot.sendMessage(chatId, `You rejected the transaction`)
+        return
+      }
+
+      bot.sendMessage(chatId, `Unknown error happened`)
+    })
+    .finally(() => connector.pauseConnection())
+
+  let deeplink = ''
+  const walletInfo = wallets.find(
+    (wallet) => wallet.name === connector.wallet!.device.appName
+  )
+  if (walletInfo && isWalletInfoRemote(walletInfo)) {
+    deeplink = walletInfo.universalLink
+  }
+
+  await bot.sendMessage(
+    chatId,
+    `Open ${connector.wallet!.device.appName} and confirm transaction`,
+    {
+      reply_markup: {
+        inline_keyboard: [
+          [
+            {
+              text: 'Open Wallet',
+              url: deeplink,
+            },
+          ],
+        ],
+      },
+    }
+  )
 }
